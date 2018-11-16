cc3d66cf257fe033b585122b7240e6576cfc141f
unknown <orthogonaltrajectory@gmail.com>
2018-11-15 23:00:37 -0900

    Canvas displays spinning sphere

--- js/occlusionControl.js
+++ js/occlusionControl.js
@@ -223,8 +223,185 @@ function begin() {
       queryInProgress: false,
       occluded: false
     };
-
     console.log("Sphere Calculated");
+    utils.xformMatrix(spheres[sphereCount].modelMatrix, spheres[sphereCount].translate);
   }
 
+  // sphereCountElement.innerHTML = spheres.lenth;
+
+
+  // CAMERA //
+
+  var projMatrix = mat4.create();
+  mat4.perspective(projMatrix, Math.PI / 2, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 10.0);
+
+  var viewMatrix = mat4.create();
+  var eyePosition = vec3.fromValues(0, 0, 5);
+  mat4.lookAt(viewMatrix, eyePosition, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+
+  var viewProjMatrix = mat4.create();
+  mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
+
+  // BIRDS EYE CAMERA
+  var projMatrixAssistCam = mat4.create();
+  mat4.perspective(projMatrixAssistCam, Math.PI / 2, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 10.0);
+
+  var viewMatrixAssistCam = mat4.create();
+  var eyePositionAssistCam = vec3.fromValues(0, 5, 0);
+  mat4.lookAt(viewMatrixAssistCam, eyePositionAssistCam, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, -1));
+
+  var viewProjMatrixAssistCam = mat4.create();
+  mat4.multiply(viewProjMatrixAssistCam, projMatrixAssistCam, viewMatrixAssistCam);
+
+  gl.useProgram(hudProgram);
+  gl.uniformMatrix4fv(hudViewProjectionLocation, false, viewProjMatrix);
+  gl.useProgram(null);
+
+  var lightPosition = vec3.fromValues(1, 1, 40);
+
+  var modelMatrix = mat4.create();
+  var rotateXMatrix = mat4.create();
+  var rotateYMatrix = mat4.create();
+
+  var sceneUniformData = new Float32Array(24);
+  sceneUniformData.set(viewProjMatrix);
+  sceneUniformData.set(eyePosition, 16);
+  sceneUniformData.set(lightPosition, 20);
+
+  var sceneUniformBuffer = gl.createBuffer();
+  gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, sceneUniformBuffer);
+  gl.bufferData(gl.UNIFORM_BUFFER, sceneUniformData, gl.STATIC_DRAW);
+
+  var assistCamUniformData = new Float32Array(24);
+  assistCamUniformData.set(viewProjMatrixAssistCam);
+  assistCamUniformData.set(eyePositionAssistCam);
+  assistCamUniformData.set(lightPosition, 20);
+
+  var assistCamUniformBuffer = gl.createBuffer();
+  gl.bindBufferBase(gl.UNIFORM_BUFFER, 1, assistCamUniformBuffer);
+  gl.bufferData(gl.UNIFORM_BUFFER, assistCamUniformData, gl.STATIC_DRAW);
+
+
+  // DEPTH DETERMINATION
+
+  var sortPositionA = vec4.create();
+  var sortPositionB = vec4.create();
+
+  var sortModelView = mat4.create();
+
+  function depthSort(a, b) {
+    vec4.set(sortPositionA, a.tranlate[0], a.tranlate[1], a.tranlate[2], 1.0);
+    vec4.set(sortPositionB, b.tranlate[0], b.tranlate[1], b.tranlate[2], 1.0);
+
+    mat4.mul(sortModelView, viewMatrix, a.modelMatrix);
+    vec4.transformMat4(sortPositionA, sortPositionA, sortModelView);
+    mat4.mul(sortModelView, viewMatrix, b.modelMatrix);
+    vec4.transformMat4(sortPositionB, sortPositionB, sortModelView);
+
+    return sortPositionB[2] - sortPositionA[2];
+  }
+
+
+  // TEXTURE //
+
+  var image = new Image();
+  var firstFrame = true;
+  var occludedSpheres = 0;
+
+  image.onload = function() {
+    var texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+
+    var levels = Math.floor(Math.log2(Math.max(this.width, this.height))) + 1;
+    gl.texStorage2D(gl.TEXTURE_2D, levels, gl.RGBA8, image.width, image.height);
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, image.width, image.height, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.useProgram(program);
+    gl.uniform1i(drawTexLocation, 0);
+    gl.useProgram(null);
+
+    var rotationMatrix = mat4.create();
+    var sphere, boundingBox;
+    var samplesPassed;
+
+    var i;
+
+    function draw() {
+      occludedSpheres = 0;
+
+      spheres.sort(depthSort);
+
+      gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+      gl.clearColor(0, 0, 0, 1);
+      gl.enable(gl.DEPTH_TEST);
+      gl.colorMask(true, true, true, true);
+      gl.depthMask(true);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      for(i = 0; i < NUM_SPHERES; ++i) {
+        sphere = spheres[i];
+        boundingBox = sphere.boundingBox;
+
+        sphere.rotate[1] += 0.003;
+
+        utils.xformMatrix(sphere.modelMatrix, sphere.translate, null, sphere.scale);
+        mat4.fromYRotation(rotationMatrix, sphere.rotate[1]);
+        mat4.multiply(sphere.modelMatrix, rotationMatrix, sphere.modelMatrix);
+
+
+        // OCCLUSION //
+
+
+        gl.colorMask(false, false, false, false);
+        gl.depthMask(false);
+        gl.useProgram(boundingBoxProgram);
+        gl.bindVertexArray(sphere.boundingBoxVertexArray);
+        gl.uniformMatrix4fv(boundingBoxModelMatrixLocation, false, sphere.modelMatrix);
+
+        // QUERY FOR OCCLUSION //
+
+        if(sphere.queryInProgress && gl.getQueryParameter(sphere.query, gl.QUERY_RESULT_AVAILABLE)) {
+          sphere.occluded = !gl.getQueryParameter(sphere.query, gl.QUERY_RESULT);
+          if(sphere.occluded) {
+            occludedSpheres++;
+          }
+          sphere.queryInProgress = false;
+        }
+
+        // Initiate query and draw bounding box of the shape
+        if(!sphere.queryInProgress) {
+          gl.beginQuery(gl.ANY_SAMPLES_PASSED_CONSERVATIVE, sphere.query);
+          gl.drawArrays(gl.TRIANGLES, 0, sphere.boundingBoxNumVertices);
+          gl.endQuery(gl.ANY_SAMPLES_PASSED_CONSERVATIVE);
+          sphere.queryInProgress = true;
+        }
+      // } else {
+      //   sphere.occluded = false;
+      // }
+
+      if(!sphere.occluded) {
+        gl.colorMask(true, true, true, true);
+        gl.depthMask(true);
+        gl.useProgram(program);
+        gl.bindVertexArray(sphere.vertexArray);
+
+        gl.uniformMatrix4fv(drawModelMatrixLocation, false, sphere.modelMatrix);
+
+        gl.drawElements(gl.TRIANGLES, sphere.numElements, gl.UNSIGNED_SHORT, 0);
+      }
+    }
+    // occludedSpheresElement.innerHTML = occludedSpheres;
+    requestAnimationFrame(draw);
+  }
+  requestAnimationFrame(draw);
+}
+image.src = "../img/khronos_webgl.png";
 }
